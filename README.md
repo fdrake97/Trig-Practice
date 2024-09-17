623defeab7c4ec104eea6260476cfb07bd593e38
BurritoKitten <burritokitten7@gmail.com>
2024-09-17 09:36:40 -0400

    Full rewrite. Mostly functional.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Play game here: https://burritokitten.github.io/Trig-Practice/welcome.html
+Play game at https://burritokitten.github.io/Trig-Dev/welcome.html
+
+Hey there! a few notes about the game:  
+Cookies are used to keep track of your name and highscore.  
+A 10 second penalty is incurred for pausing to prevent cheating.  
+Score is calculated with the following formula:  
+> score = (score*0.95 + 100000/(val)*(1+(streak/10)));  
+
+Where val is the time spent on the last problem in ms.  
+The program keeps a rolling tally of your score, and when you submit the leaderboard database recalculates your score.
--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-const sinArray = [
+const sinArray_ = [
   ['0°', '0', '0'],
   ['30°', 'π/6', '1/2'],
   ['45°', 'π/4', 'r2/2'],
@@ -17,175 +17,178 @@ const sinArray = [
   ['330°', '11π/6', '-1/2'],
   ['360°', '2π', 'NAN']
 ];
-var startTime = new Date().getTime() / 1000;
-var totalAnswered = 0;
-var totalCorrect = 0;
-var currentStreak = 0;
-var currentAnswer;
-var lastQuestion;
-var currentScore = 0;
-var bestScore = 0;
-var timeOfLastCorrect = startTime;
-var root = document.querySelector(':root');
-var lifetimeCorrect = 0;
-var degVsRad;
+const penalty_ = 10000;
+var startTime_ = time();
+var questionTime_ = 0;
+var pauseTime_ = 0;
+var passedTime_ = 0;
+var correctAnswer_;
+var times_ = "";
+var question_ = "An error has occurred";
+var answer_ = 0;
+var answered_ = 0;
+var score_ = 0;
+var streak_ = 0;
+let questionNum_;
+let questionType_;
 
-function scanCookies() {
-  if (getCookie('set') != 'true') {
-    window.location.href = './options.html';
+function toggleSettings() {
+  var x = document.getElementById("settings");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-  cookie = getCookie("backgroundColor")
-  if (cookie != "") {
-    console.log(cookie)
-    document.body.style.backgroundColor = cookie;
-    var all = document.getElementsByTagName("input")
-    for (var i = 0, max = all.length; i < max; i++) {
-      all[i].style.backgroundColor = cookie;
-    }
-  }
-  cookie = getCookie("textColor")
-  if (cookie != "") {
-    console.log(cookie)
-    var all = document.getElementsByTagName("*");
+};
 
-    for (var i = 0, max = all.length; i < max; i++) {
-      all[i].style.color = cookie;
-    }
-  }
-  cookie = getCookie("bestScore");
-  if (cookie != "") { bestScore = cookie; }
-  cookie = getCookie("lifetimeCorrect");
-  if (cookie != "") { lifetimeCorrect = cookie; }
-}
+function pause() {
+  document.getElementById("app").style.display = "none";
+  document.getElementById("pause").style.display = "block";
+  pauseTime = time();
+};
 
-function setCookie(cname, cvalue) {
-  const d = new Date();
-  d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
-  let expires = "expires=" + d.toUTCString();
-  let cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  console.log("Cookie set to: " + cookie)
-  document.cookie = cookie;
-}
+function unpause() {
+  document.getElementById("app").style.display = "block";
+  document.getElementById("pause").style.display = "none";
+  startTime += ((time() - pauseTime_) - penalty_);
+  questionTime += ((time() - pauseTime_) - penalty_);
 
-function getCookie(cookieName) {
-  let name = cookieName + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let cookieArray = decodedCookie.split(';');
-  console.log(cookieArray);
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookieElement = cookieArray[i];
-    while (cookieElement.charAt(0) == ' ') {
-      cookieElement = cookieElement.substring(1);
-    }
-    if (cookieElement.indexOf(name) == 0) {
-      return cookieElement.substring(name.length);
-    }
-  }
-  console.log("Failed to find cookie " + cookieName);
-  return "";
-}
-
-function newQuestion() {
-  //generate question
-  let currentQuestion = getCookie('full_circle_toggle') == 'true' ? Math.floor(Math.random() * 17) : Math.floor(Math.random() * 9);
-  let currentSinState = Math.floor(Math.random() * 2)
-  //check if qestion is different from last (ignores rad/deg)
-  if (currentQuestion + 17 * (currentSinState) == lastQuestion) {
-    newQuestion();
-    return;
-  }
-  //choose degree or radian
-  degVsRad = getCookie('radian_toggle') == 'true' ? (getCookie('degree_toggle') == 'true' ? Math.floor(Math.random() * 2) : 1) : 0;
-  //actually select question
-  let question = currentSinState == 0 ? "sin(" + sinArray[currentQuestion][degVsRad] + ")" : "cos(" + sinArray[currentQuestion][degVsRad] + ")";
-  //choose matching answer
-  currentAnswer = sinArray[(currentQuestion + 4 * currentSinState + 16) % 16][2];
-  console.log(currentAnswer);
-  //update last question
-  lastQuestion = currentQuestion + 17 * (currentSinState);
-  //SET QUESTION//
-  document.getElementById('question').innerText = question;
-  //update image//
-  loadImage();
-}
+};
 
 function checkAnswer() {
-  //collect answer
-  var userAnswer = document.getElementById("answer").value.toLowerCase().trim();
-  //check if answer is an option
-  if (isAnswerOption(userAnswer)) {
-    //increment total answers
-    totalAnswered++;
-    //check if answer is right
-    if (userAnswer == currentAnswer) {
-      //increase counts, update score
-      timeOfLastCorrect = new Date().getTime() / 1000;
-      currentStreak++;
-      totalCorrect++;
-      lifetimeCorrect++;
-      setCookie('lifetimeCorrect', lifetimeCorrect);
-      currentScore = Math.floor(totalCorrect ** 2 / (timeOfLastCorrect - startTime) * 1000);
-      if (currentScore > bestScore) {
-        bestScore = currentScore;
-        setCookie("bestScore", bestScore)
-      }
-      document.getElementById("result").innerText = "Correct! Well done!";
-      newQuestion();
-    }
-    else {
-      currentStreak = 0;
-      document.getElementById("result").innerText = "Incorrect. Please try again."
-    }
-  }
-  else {
-    document.getElementById("result").innerText = "Please enter a valid answer option.";
-  }
-  document.getElementById('score').innerHTML = scoreGen();
-  document.getElementById("answer").value = "";
-}
+  answer = (document.getElementById("answer").value.toLowerCase().trim());
+  if (!isAnswerOption(answer_)) { notAnAnswerOption(); return; }
+  answered_++;
+  passedTime_ = time() - questionTime_;
+  if (answer_ == correctAnswer_) { isCorrectAnswer(); }
+  else { incorrectAnswer(); }
 
-function isAnswerOption(answer) {
+  console.log(answer_);
+  console.log(correctAnswer_);
+  newQuestion();
+};
+
+function isAnswerOption(answer_) {
   for (x = 0; x < 16; x++) {
-    if (sinArray[x][2] == answer) { return true; }
+    if (sinArray_[x][2] == answer_) { return true; }
   }
   return false;
 }
 
-function scoreGen() {
-  return '<p>Score: ' + currentScore + '<br>Streak: ' + currentStreak + '<br>Average time: ' + Math.floor((timeOfLastCorrect - startTime) / totalCorrect * 100) / 100 + ' seconds<br><br>Total correct: ' + lifetimeCorrect + '<br>Best score: ' + bestScore + '<p>';
+function isCorrectAnswer() {
+  streak_++;
+  score_ = (score_ * 0.95 + 100000 / passedTime_ * (1 + (streak_ / 10)));
+  logTime(passedTime_);
+  document.getElementById("result").innerText = "Correct! Well done!";
+};
+
+function incorrectAnswer() {
+  streak_ = 0;
+  score_ = score_ * 0.95;
+  logTime(-passedTime_);
+  document.getElementById("result").innerText = "Incorrect. Please try again."
+};
+
+function notAnAnswerOption() {
+  document.getElementById("result").innerText = "Please enter a valid answer option.";
+};
+
+function newQuestion() {
+  generateQuestion();
+  document.getElementById('question').innerText = question_;
+  updateStats();
+};
+
+function generateQuestion() {
+  var questionNum_ = random(getCookie("full_circle") == "0" ? 9 : 17);
+  questionType_ = getCookie('type') == 'deg' ? 0 : getCookie('type') == 'rad' ? 1 : random(2);
+  var trigVar_ = random(2);
+  question_ = (trigVar_ == 0 ? "sin " : "cos ") + sinArray_[questionNum_][questionType_];
+  correctAnswer_ = sinArray_[(questionNum_ + trigVar_ * 4) % 16][2];
+  clearImage();
+  if (getCookie("image") != "0") {
+    addImage(
+      (getCookie("fullCircle") == "1" ? 1 : 0) * 0b100 +
+      questionType_ * 0b010 +
+      (getCookie("coords") == "1" ? 1 : 0)) * 0b001;
+  }
+  questionTime_ = time();
+};
+
+function addImage(mask_) {
+  var maskStr_ = "";
+  for (i = 0; i < 3; i++) {
+    maskStr_ += (mask_ % 2 == 0) ? "0" : "1";
+    mask_ = mask_ >> 1;
+  }
+  console.log(maskStr_);
+  document.getElementById("0b"+maskStr_).style.display = "block";
 }
 
-function loadImage() {
-  if (getCookie('circle_toggle') == 'true'){return;}
-  mask = 0b0000;
-  if (getCookie('full_circle_toggle') == 'true') { mask = mask | 0b100; }
-  mask = mask | degVsRad*2;
-  if (getCookie('coordinate_toggle') == 'true') { mask = mask | 0b001; }
-  console.log(mask);
-  switch (mask) {
-    case 0b101:
-      document.getElementById('image').src = 'full_unit_circle_black_no_coords.svg';
-      break;
-    case 0b100:
-      document.getElementById('image').src = 'full_unit_circle_black.svg';
-      break;
-    case 0b111:
-      document.getElementById('image').src = 'full_unit_circle_rad_no_coords.svg';
-      break;
-    case 0b110:
-      document.getElementById('image').src = 'full_unit_circle_rad.svg';
-      break;
-    case 0b001:
-      document.getElementById('image').src = 'half_unit_circle_black_no_coords.svg';
-      break;
-    case 0b000:
-      document.getElementById('image').src = 'half_unit_circle_black.svg';
-      break;
-    case 0b011:
-      document.getElementById('image').src = 'half_unit_circle_rad_no_coords.svg';
-      break;
-    case 0b010:
-      document.getElementById('image').src = 'half_unit_circle_rad.svg';
-      break;
-  }
+function clearImage() {
+  document.getElementById("0b101").style.display = "none";
+  document.getElementById("0b100").style.display = "none";
+  document.getElementById("0b111").style.display = "none";
+  document.getElementById("0b110").style.display = "none";
+  document.getElementById("0b001").style.display = "none";
+  document.getElementById("0b000").style.display = "none";
+  document.getElementById("0b011").style.display = "none";
+  document.getElementById("0b010").style.display = "none";
 }
+
+function updateStats() {
+  if (getCookie("highscore") == "") { setCookie("highscore", "0"); }
+  document.getElementById("highscore").innerText = Math.trunc(parseInt(getCookie("highscore")));
+  document.getElementById("score").innerText = Math.trunc(score_);
+  document.getElementById("avgTime").innerText = ((time() - startTime_) / 1000) / answered_;
+  document.getElementById("highscore1").innerText = Math.trunc(parseInt(getCookie("highscore")));
+  document.getElementById("score1").innerText = Math.trunc(score_);
+  document.getElementById("avgTime1").innerText = ((time() - startTime_) / 1000) / answered_;
+  console.log((time() - startTime_) / 1000);
+}
+
+function random(number_) {
+  return Math.floor(Math.random() * number_);
+};
+
+function logTime(input_) {
+  console.log("logging " + input_);
+  times_ += (input_ + " ");
+  console.log(parseInt(getCookie("highscore")));
+  if (score_ > parseInt(getCookie("highscore"))) {
+    setCookie("highscore", score_);
+    setCookie("bestGame", times_);
+    console.log("logged " + times_);
+  }
+};
+
+function time() {
+  return new Date().getTime();
+};
+
+function setCookie(cookieName_, cookieValue_) {
+  const d = new Date();
+  d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+  let expires_ = "expires=" + d.toUTCString();
+  let cookie_ = cookieName_ + "=" + cookieValue_ + ";" + expires_ + ";path=/";
+  console.log("Cookie set to: " + cookie_)
+  document.cookie = cookie_;
+};
+
+function getCookie(cookieName_) {
+  let name_ = cookieName_ + "=";
+  let decodedCookie_ = decodeURIComponent(document.cookie);
+  let cookieArray_ = decodedCookie_.split(';');
+  console.log(cookieArray_);
+  for (let i = 0; i < cookieArray_.length; i++) {
+    let cookieElement_ = cookieArray_[i];
+    while (cookieElement_.charAt(0) == ' ') {
+      cookieElement_ = cookieElement_.substring(1);
+    }
+    if (cookieElement_.indexOf(name_) == 0) {
+      return cookieElement_.substring(name_.length);
+    }
+  }
+  console.log("Failed to find cookie " + cookieName_);
+  return "";
+};
